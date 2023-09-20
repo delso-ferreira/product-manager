4c06b3df160b84c996de9642e87e0dd17559c52c
delso-ferreira <delsofelipe@gmail.com>
2023-09-20 19:50:05 -0300

    add req 07

--- backend/src/middlewares/products.middleware.js
+++ backend/src/middlewares/products.middleware.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const productsModel = require('../models/products.model');
 
 const MIN_NAME = 5;
 
@@ -11,7 +12,7 @@ const validateProductName = (req, res, next) => {
 
     if (name.length < MIN_NAME) {
         return res.status(422).json({
-            message: '"name" length must be at least 5 characters long'
+            message: '"name" length must be at least 5 characters long',
         });
     }
 
@@ -20,19 +21,37 @@ const validateProductName = (req, res, next) => {
 
 const validateProduct = (req, res, next) => {
     const { name } = req.body;
+
     const schema = Joi.object({
         name: Joi.string().min(5).required(),
     });
+
     const { error } = schema.validate({ name });
     if (error) {
         const statusCode = error.details[0].type === 'string.min' ? 422 : 400;
         return res.status(statusCode)
             .json({ message: error.message });
     }
+
+    next();
+};
+
+const validateProductId = async (req, res, next) => {
+    const { id } = req.params;
+
+    const product = await productsModel.getProductById(id);
+
+    if (!product || product.length === 0) {
+        return res.status(404).json({ message: 'Product not found' });
+    }
+
+    req.product = product;
+
     next();
 };
 
 module.exports = {
     validateProductName,
     validateProduct,
+    validateProductId,
 };
--- backend/src/routes/products.route.js
+++ backend/src/routes/products.route.js
@@ -1,10 +1,19 @@
 const route = require('express').Router();
 const { productsController } = require('../controllers');
-const { validateProductName, validateProduct } = require('../middlewares/products.middleware');
+
+const { validateProductName,
+    validateProduct,
+    validateProductId } = require('../middlewares/products.middleware');
 
 route.get('/', productsController.getAllProducts);
 route.get('/:id', productsController.getProductById);
 route.post('/', validateProductName, productsController.createProduct);
-route.put('/:id', validateProductName, validateProduct, productsController.updateProduct);
+route.put(
+    '/:id',
+    validateProductName,
+    validateProduct,
+    validateProductId,
+    productsController.updateProduct,
+);
 
 module.exports = route;
